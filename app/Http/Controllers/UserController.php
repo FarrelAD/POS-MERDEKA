783967bfa6c96745fb195aff29a673ed2ac52273
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-03-04 22:31:26 +0700

    add: implementation of attribute changes in eloquent orm

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,12 +9,23 @@ class UserController extends Controller
 {
     public function index()
     {
-        $user = UserModel::firstOrNew([
-            'username' => 'manager333',
-            'nama' => 'manager TIGA THREE',
+        $user = UserModel::create([
+            'username' => 'manager11',
+            'nama' => 'manager sebelas',
             'password' => Hash::make('12345'),
             'level_id' => 2
         ]);
+
+        $user->username = 'manager12';
+
+        $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('nama');
+
+        dd($user->wasChanged(['nama', 'username']));
 
         return view('user', ['data' => $user]);
     }
