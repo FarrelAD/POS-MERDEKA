c250b7008a54abfca30c1df37b5fde4a51bf5374
Farrel Augusta Dinata <farrel.apeiron@gmail.com>
2025-03-04 21:44:09 +0700

    add: new attribute `fillable` in `UserModel` class and try to implement it in `UserController`

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,28 +3,20 @@
 namespace App\Http\Controllers;
 
 use App\Models\UserModel;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function index()
     {
-        // $data = [
-        //     'username' => 'customer 1',
-        //     'nama' => 'Pelanggan',
-        //     'password' => Hash::make('12345'),
-        //     'level_id' => 4
-        // ];
-
-        // UserModel::insert($data);
-
-
         $data = [
-            'nama' => 'Pelanggan pertama'
+            'username' => 'manager 3',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+            'level_id' => 2
         ];
 
-        $user = UserModel::where('username', 'customer 1')->update($data);
+        UserModel::create($data);
 
         $user = UserModel::all();
 
